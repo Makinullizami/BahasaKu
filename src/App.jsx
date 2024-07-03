fed176d84841de736480ec7160a8e6d4580cad60
Makinullizami <agimbal65@gmail.com>
2024-07-03 16:47:45 +0700

    membuat header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, StatusBar, View} from 'react-native';
 import React from 'react';
 
 const App = () => {
+  // Definisikan nilai-nilai status bar
+  const statusBarStyle = 'default'; // Bisa juga 'light-content' atau 'default'
+  const statusBarTransition = 'slide'; // Bisa juga 'slide'
+  const hidden = false; // Bisa juga 'true' untuk menyembunyikan status bar
+
   return (
-    <View>
-      <Text>App</Text>
+    <View style={{Color: '#fffff'}}>
+      <StatusBar
+        animated={true}
+        backgroundColor="#FF7D29"
+        barStyle={statusBarStyle}
+        showHideTransition={statusBarTransition}
+        hidden={hidden}
+      />
+      <View style={styles.header}>
+        <Text style={styles.txt}>BahasaKu</Text>
+      </View>
     </View>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#FF7D29',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+  },
+  txt: {
+    fontSize: 45,
+    textAlign: 'center',
+    justifyContent: 'center',
+    color: '#ffffff',
+    fontWeight: 'bold',
+  },
+});
